7f33a41854d75e958a1057b27f330485993245dd
Juan Carlos Tede Garcia <diego112018@gmail.com>
2025-09-19 17:57:09 -0600

    [Add] Agregue funciones nuevas

---
+++ app/utils/utils.py
@@ -0,0 +1,32 @@
+import json
+import pydantic
+import enum
+
+from datetime import datetime
+
+class TaskStatus(enum.Enum):
+    PENDING = "PENDING"
+    IN_PROGRESS = "In Progress"
+    COMPLETED = "COMPLETED"
+
+class Task(pydantic.BaseModel):
+    id: int
+    title: str
+    description: str
+    status: str = TaskStatus.PENDING.value
+    timestamp: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
+
+def get_tasks(path:str="./data/data.json"):
+    try:
+        with open(path, "r") as file:
+            data = json.load(file)
+            return data["tasks "]
+    except Exception as e:
+        print(f"Error loading tasks: {e}")
+      
+def save_task(task:list, path:str="./data/data.json"):
+    try:
+        with open(path, "w") as f:
+            json.dump({"tasks ": task}, f, indent=4)
+    except Exception as e:
+        print(f"Error saving tasks in {path}: {e}")
